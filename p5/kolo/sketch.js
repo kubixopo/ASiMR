26f6f877f5e390ca89ab3845d3914d30b369018d
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2019-10-04 22:08:02 +0200

    p5js

---
+++ p5/kolo/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500,500)
+  r=1
+  // put setup code here
+}
+
+function draw() {
+  eellipse(100,100,r)
+
+  if(r>100){
+    r=2
+  }else{
+    r++
+  }
+
+  // put drawing code here
+}
