c4e240ab764f467d290899ef20e289ab424df0fb
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2019-10-04 23:15:38 +0200

    js

--- p5/kolo/sketch.js
+++ p5/kolo/sketch.js
@@ -1,12 +1,12 @@
 function setup() {
   createCanvas(500,500);
   r=1;
-  background('yellow');
+  background(240, 248, 255, 0.774);
   // put setup code here
 }
 
 function draw() {
-  background('yellow');
+  background(240, 248, 255, 0.774);
   ellipse(100,100,r);
   if(r>100){
     r=2;
