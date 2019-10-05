82bc5fe2cbd8dc6ad2b54f44fefc47b02fb23f61
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2019-10-05 22:11:37 +0200

    Update sketch.js

--- p5/gierka2/sketch.js
+++ p5/gierka2/sketch.js
@@ -4,49 +4,79 @@ function setup() {
   tab.push(new kwadrat(100,100,300))
   // put setup code here
   //noStroke()
+ /* col1=color('rgba(254,167,5,0.6)')
+  col2=color('rgba(248,5,23,0.6)')
+  col3=color('rgba(20,72,169,0.6)')
+  col4=color('rgba(33,210,5,0.6)')*/
+
+  /*col1=color('rgba(99%,50%,2%,60)')
+  col2=color('rgba(85%,2%,10%,60)')
+  col3=color('rgba(20,72,169,60)')
+  col4=color('rgba(33,210,5,60)')*/
 }
+let col1,col2,col3,col4
 let score=1
 let tab=[]
 function draw() {
-  background(170, 57, 57,0.6);
- // fill('white')
+ background(170, 57, 57,0.6);
+ if(frameCount%60==0){
+  fill(255,255,255)
   //rect(100,100,300,300)
+ }
+ fill('white')
+  //fill(255,255,255,0.6)
+  rect(100,100,300,300)
   fill('black')
   text(score,width/2,50)
   score=tab.length
 for(i of tab){
  //if(i.state){
-  // fill(100,100,100,0.5)
-
-  //rect(i.x,i.y,i.a,i.a)
-
+   //fill(100,100,100,0.5)
+//if(i.e==1){
+ // fill(255,255,255,0.6)
+ //rect(i.x,i.y,i.a,i.a)
+//}
   if(i.a==300){
-    fill(254,167,5,0.6)
+   // if(i.e<240){
+    //  fill(254,167,5,0.6)
+   // }else{
+    fill(254,167,5,i.e)
+   // }
   }else if(i.a==150){
-    fill(248,5,23,0.6)
+   // if(i.e<240){
+    //  fill(248,5,23,0.6)
+   // }else{
+    fill(248,5,23,i.e)
+   // }
     
   }else if(i.a==75){
-    
-    fill(20,72,169,0.6)
+    //if(i.e<240){
+     // fill(20,72,169,0.6)
+   // }else{
+    fill(20,72,169,i.e)
+   // }
   }else if(i.a==37.5){
     
-    fill(33,210,5,0.6)
+    fill(33,210,5,i.e)
   }else if(i.a==18.75){
-  fill(165,4,165,0.6)
+  fill(165,4,165,i.e)
 
   }else if(i.a==9.375){
-    fill(254,76,5,0.6)
+    fill(254,76,5,i.e)
     
   }else if(i.a==4.6875){
-    fill(204-170,245-57,5,0.6)
+    fill(204,245,5,i.e)
     
   }else if(i.a==2.34375){
-    fill(15,94,165,0.6)
+    fill(15,94,165,i.e)
     
   }
 
 
   i.drawa()
+ if(i.e<60){
+  i.e++
+ }
   //i.mc()
   
 }
@@ -59,6 +89,7 @@ this.x=x
 this.y=y
 this.state=true
 this.a=a
+this.e=30
 this.drawa=function(){
 
 
