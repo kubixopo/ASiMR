4df97ea3da286a210b78d480c12ca815231ac8ee
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2019-10-05 23:17:55 +0200

    Update sketch.js

--- p5/gierka2/sketch.js
+++ p5/gierka2/sketch.js
@@ -1,4 +1,5 @@
 function setup() {
+ // blendMode(REPLACE)
   createCanvas(500,500);
   r=1;
   tab.push(new kwadrat(100,100,300))
@@ -17,15 +18,30 @@ function setup() {
 let col1,col2,col3,col4
 let score=1
 let tab=[]
+let f=true
 function draw() {
- background(170, 57, 57,0.6);
+  //background(170, 57, 57,0.0);
+ // clear()
+  if(f){
+  fill(170, 57, 57,0.1)
+  //  fill(255, 142, 142,60)
+  noStroke()
+  rect(0,0,500,100)
+  rect(0,100,100,400)
+  rect(100,400,400,100)
+  rect(400,100,100,300)
+  stroke(1)
+  }
+ 
  if(frameCount%60==0){
-  fill(255,255,255)
+  //fill(255,255,255)
   //rect(100,100,300,300)
  }
  fill('white')
   //fill(255,255,255,0.6)
-  rect(100,100,300,300)
+  //rect(100,100,300,300)
+  
+  rect(width/2-15,35,50,20)
   fill('black')
   text(score,width/2,50)
   score=tab.length
@@ -34,32 +50,32 @@ for(i of tab){
    //fill(100,100,100,0.5)
 //if(i.e==1){
  // fill(255,255,255,0.6)
- //rect(i.x,i.y,i.a,i.a)
+  //rect(i.x,i.y,i.a,i.a)
 //}
   if(i.a==300){
-   // if(i.e<240){
-    //  fill(254,167,5,0.6)
+    //if(i.e<120){
+     // fill(254,167,5,0.6)
    // }else{
     fill(254,167,5,i.e)
    // }
   }else if(i.a==150){
-   // if(i.e<240){
+   // if(i.e<120){
     //  fill(248,5,23,0.6)
    // }else{
     fill(248,5,23,i.e)
    // }
     
   }else if(i.a==75){
-    //if(i.e<240){
-     // fill(20,72,169,0.6)
-   // }else{
-    fill(20,72,169,i.e)
+   // if(i.e<120){
+    //  fill(20,72,169,0.6)
+    //}else{
+    fill(20,72,250,i.e)
    // }
   }else if(i.a==37.5){
     
-    fill(33,210,5,i.e)
+    fill(3,250,5,i.e)
   }else if(i.a==18.75){
-  fill(165,4,165,i.e)
+  fill(145,4,65,i.e)
 
   }else if(i.a==9.375){
     fill(254,76,5,i.e)
@@ -74,9 +90,9 @@ for(i of tab){
 
 
   i.drawa()
- if(i.e<60){
-  i.e++
- }
+  if(i.e<2){
+ i.e+=0.1
+  }
   //i.mc()
   
 }
@@ -89,7 +105,7 @@ this.x=x
 this.y=y
 this.state=true
 this.a=a
-this.e=30
+this.e=2
 this.drawa=function(){
 
 
@@ -127,6 +143,18 @@ for(i of tab){
     let r=new kwadrat(i.x+i.a/2,i.y+i.a/2,i.a/2)
     i.state=false
    // s=true
+  // background('white');
+   //background(255,255,255,1);
+   //clear()
+   //redraw();
+   f=false
+   /*fill(170, 57, 57,160)
+   rect(0,0,500,100)
+   rect(0,100,100,400)
+   rect(100,400,400,100)
+   rect(400,100,100,300)*/
+  // fill()
+   //rect(i.x,i.y,i.a,i.a)
     tab.push(q,w,e,r)
     
    break;
