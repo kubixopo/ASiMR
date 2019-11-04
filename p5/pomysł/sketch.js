854c137a0fb9c89b8c389099b4562eb24c352466
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2019-11-04 10:23:09 +0100

    p5js

--- p5/pomysł/sketch.js
+++ p5/pomysł/sketch.js
@@ -1,8 +1,11 @@
 function setup(){
 
 createCanvas(500,500)
+//frameRate(120)
 background(120)
 wyp.push(new wypelnienie(250,250,1),new wypelnienie(250,250,2),new wypelnienie(250,250,3),new wypelnienie(250,250,4))
+
+wyp.push(new wypelnienie(250,250,5),new wypelnienie(250,250,6),new wypelnienie(250,250,7),new wypelnienie(250,250,8))
 }
 let tab=[]
 let wyp=[]
@@ -17,8 +20,8 @@ if(mouseIsPressed){
 tab.push(new kwadrat(mouseX-(mouseX%10),mouseY-(mouseY%10)))
 }
 
-if(keyIsDown(32)){
-  
+//if(keyIsDown(32)){
+  if(frameCount>30){
  runda=true
   for(i of wyp){
     
@@ -79,14 +82,23 @@ function wypelnienie(x,y,k){
         //if(dist(i.x,i.y,this.x,this.y)>15 || dist(i.x,i.y,this.x,this.y)==0){
 
       
-if(this.k==3)
-    wyp.push(new wypelnienie(this.x+10,this.y+10,3))  
+    if(this.k==3)
+      wyp.push(new wypelnienie(this.x+10,this.y+10,3))  
     if(this.k==4)
      wyp.push(new wypelnienie(this.x-10,this.y-10,4)) 
-     if(this.k==1)
+    if(this.k==1)
      wyp.push(new wypelnienie(this.x-10,this.y+10,1)) 
-     if(this.k==2)
+    if(this.k==2)
       wyp.push(new wypelnienie(this.x+10,this.y-10,2)) 
+
+    if(this.k==5)
+      wyp.push(new wypelnienie(this.x+20,this.y,5))  
+    if(this.k==6)
+     wyp.push(new wypelnienie(this.x-20,this.y,6)) 
+    if(this.k==7)
+     wyp.push(new wypelnienie(this.x,this.y+20,7)) 
+    if(this.k==8)
+      wyp.push(new wypelnienie(this.x,this.y-20,8)) 
     this.s=1
         }
     //}
